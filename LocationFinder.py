5ef8261048096acc8ac2f42a28510c4bb9d72a25
ma-casali <60496643+ma-casali@users.noreply.github.com>
2020-02-05 12:22:52 -0800

    Update LocationFinder.py

--- LocationFinder.py
+++ LocationFinder.py
@@ -6,12 +6,10 @@ from mpl_toolkits import mplot3d as plt3d
 
 
 #---functions----------------------
-def Energymaker(SM,number): #function to create theoretical intensities as a function of test distances from microphone.
+def Energymaker(SM,r): #function to create theoretical intensities as a function of test distances from microphone.
 
 	#test path 
-	r_x = np.linspace(-10,10,num = number)
-	r_y = r_x
-	r_z = r_x
+	r_x, r_y, r_z, number = r
 
 	#distance from each microphone
 	def d(SM,r_x,r_y,r_z):
@@ -43,7 +41,7 @@ def Energymaker(SM,number): #function to create theoretical intensities as a fun
 
 	I00,I01,I10,I11,I20,I21 = I
 
-	return I, d
+	return I
 
 #---main function---
 
@@ -54,62 +52,55 @@ def position_estimate(I,SM,t,number): #estimate positions from positions of micr
 	Isetz = np.array([I[4][t],I[5][t]])
 
 	def k(I):
-		if min(I) == I[0]:
-			mult = 1
+		if max(I) == I[0]: #this process comes from solving k**2 * d1 **2 = d2 **2 for x and y
+			mult = 1 # if k is on side of d1
 		else:
-			mult = -1
+			mult = -1 # if k is on side of d2
 		return np.sqrt(min(I)/max(I)), mult
+
+	def c(R,shift):
+		return R + shift
+
+	def m(R,k,c,mult):
+		return c + mult*(R * (k**2 + 1)/(k**2 - 1))
+
+	def l_sqd(R,k):
+		return R **2 * (((k**2 + 1)/(k**2 - 1))**2 - 1)
 
 	def position(p,*data): #function to find zeros for
 		x,y,z = p
-		R1,R2,R3,k1,k2,k3,mult1,mult2,mult3 = data
-
-		def m(R,k):
-			return R* (k**2 + 1)/(k**2 - 1)
-
-		def l(R,k):
-			return R * np.sqrt(((k**2 + 1)/(k**2 - 1))**2 - 1)
-
-		m1,m2,m3 = m(R1,k1), m(R2,k2), m(R3,k3)
-		l1,l2,l3 = l(R1,k1), l(R2,k2), l(R3,k3)
+		m1,m2,m3,l1,l2,l3 = data
 
 		return(
-			(x+mult1*(m1))**2+y**2+z**2-l1**2,
-			(y+mult2*(m2))**2+x**2+z**2-l2**2,
-			(z+mult3*(m3))**2+y**2+x**2-l3**2)
+			(x+m1)**2+y**2+z**2-l1,
+			(y+m2)**2+x**2+z**2-l2,
+			(z+m3)**2+y**2+x**2-l3)
 
 	R1, R2 = [.08,.08]
 	R3 = .1325
+
 	k1, mult1 = k(Isetx)
 	k2, mult2 = k(Isety)
 	k3, mult3 = k(Isetz)
-
-	data = (R1,R2,R3,k1,k2,k3,mult1,mult2,mult3)
 	
-	pos = int(number**2.1)
-
-	if mult1 == 1:
-		pos1 = -1*pos
-	else:
-		pos1 = pos
-	if mult2 == 1:
-		pos2 = -1*pos
-	else:
-		pos2 = pos
-	if mult3 == 1:
-		pos3 = -1*pos
-	else:
-		pos3 = pos
-
+	c1,c2,c3 = c(R1,0.05),c(R2,0.05),c(R3,0.035)
+	m1,m2,m3 = m(R1,k1,c1,mult1), m(R2,k2,c2,mult2), m(R3,k3,c3,mult3)
+	l1,l2,l3 = l_sqd(R1,k1), l_sqd(R2,k2), l_sqd(R3,k3)
+	
+	data = (m1,m2,m3,l1,l2,l3)
+	bias = np.mean(np.array([np.sqrt(1/I[0][t]),np.sqrt(1/I[2][t]),np.sqrt(1/I[4][t])]))
+	pos = bias*(bias/2)
+	pos1, pos2, pos3 = (c1 + mult1*pos), (c2 + mult2*pos), (c3 + mult3*pos)
 	p = (pos1,pos2,pos3)
+
 	x,y,z = fsolve(position,p,args = data,factor = 10)
 
-	return x,y,z
+	return -1*x,-1*y,-1*z
 
 #---defining terms-----------------
 
 #position of recorders
-rec_pos = np.array([(0.21,0.05,0,0,0,0),(0,0,0.21,0.05,0,0),(0,0,0,0,0.3,0.035)]) #((x_n),(y_n),(z_n)) n = 0,1,2,3,4,5
+rec_pos = np.array([(0.05,0.21,0,0,0,0),(0,0,0.05,0.21,0,0),(0,0,0,0,0.035,0.3)]) #((x_n),(y_n),(z_n)) n = 0,1,2,3,4,5
 
 #each recorder (SM_n) position as (x,y,z)
 SM_1 = np.array([(rec_pos[0][0],rec_pos[0][1]), (rec_pos[1][0],rec_pos[1][1]),(rec_pos[2][0],rec_pos[2][1])])
@@ -119,14 +110,15 @@ SM_3 = np.array([(rec_pos[0][4],rec_pos[0][5]), (rec_pos[1][4],rec_pos[1][5]),(r
 SM = np.array([SM_1,SM_2,SM_3])
 
 #test position
-number = 100
-r_x = np.linspace(-10,10,num = number)
-r_y = r_x
-r_z = r_x
+number = 50
+r_x = np.linspace(-2*np.pi,2*np.pi,num = number)
+r_y = 100*np.cos(r_x)
+r_z = 100*np.sin(r_x)
+r = (r_x,r_y,r_z, number)
 
 #---executable code----------------
 
-I, d = Energymaker(SM,number)
+I = Energymaker(SM,r)
 t = 0
 posx, posy, posz = [[],[],[]]
 
@@ -142,22 +134,37 @@ x = np.array(posx)
 y = np.array(posy)
 z = np.array(posz)
 
-#print(x,y,z)
+x_err = 100*(r_x-x)/r_x
+y_err = 100*(r_y-y)/r_y
+z_err = 100*(r_z-z)/r_z
+
+print('average x error: ',np.median(x_err),'%')
+print('average y error: ',np.median(y_err),'%')
+print('average z error: ',np.median(z_err),'%')
+
+x_err = list(x_err)
+
+for i in x_err:
+	t = x_err.index(i)
+	if i >= 5.00:
+		print("Error greater than 5% for x at ", t, x[t],y[t],z[t])
+	if y_err[t] >= 5.00:
+		print("Error greater than 5% for y at ", t, x[t],y[t],z[t])
+	if z_err[t] >= 5.00:
+		print("Error greater than 5% for z at ", t, x[t],y[t],z[t])
+
 
 #---plotting-----------------------
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
-ax.set_xlim3d(-2, 2)
-ax.set_ylim3d(-2, 2)
-ax.set_zlim3d(-2, 2)
 
 ax.plot3D(SM_1[0],SM_1[1],SM_1[2], linestyle = 'none', marker = 'o') # mark recorders
 ax.plot3D(SM_2[0],SM_2[1],SM_2[2], linestyle = 'none', marker = 'o')
 ax.plot3D(SM_3[0],SM_3[1],SM_3[2], linestyle = 'none', marker = 'o')
 
 ax.plot3D(x,y,z, color = 'r', linestyle = 'none',marker = '.')
-ax.plot3D(r_x,r_y,r_z, 'g', linestyle = 'none', marker = '.')#test path
+ax.plot3D(r_x,r_y,r_z, 'g')#, linestyle = 'none', marker = '.')#test path
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
